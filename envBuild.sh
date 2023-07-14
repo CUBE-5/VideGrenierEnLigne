0a724db28fdbbfa6b0742eede725efbd37766c0a
lNalator <enzomidonetpro@gmail.com>
2023-07-14 23:08:42 +0200

    EM : Fixed Volume error and Dockerfile script

--- envBuild.sh
+++ envBuild.sh
@@ -5,21 +5,24 @@
 
 gitBranchCheck() {
     branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
-    if [[ $branch == main ]];
+    if [[ $branch == VID-ENV-EM ]];
         then 
         echo "Env is production"
-        docker build --build-arg isDev="false" --tag=vid-grenier-prod .
+        docker compose --env-file ./config/.env.prod -f docker-compose.yml -f ./config/docker-compose.prod.yml down
+        docker build --build-arg envi=prod --tag=vid-grenier-prod .
         docker compose --env-file ./config/.env.prod -f docker-compose.yml -f ./config/docker-compose.prod.yml up -d
 
     elif [[ $branch == staging ]];
         then
         echo "Env is staging"
-        docker build --build-arg isDev="false" --tag=vid-grenier-staging .
+        docker compose --env-file ./config/.env.staging -f docker-compose.yml -f ./config/docker-compose.staging.yml down
+        docker build --build-arg envi=staging --tag=vid-grenier-staging .
         docker compose --env-file ./config/.env.staging -f docker-compose.yml -f ./config/docker-compose.staging.yml up -d
     
     else
         echo "Env is dev"
-        docker build --build-arg isDev="true" --tag=vid-grenier-dev .
+        docker compose --env-file .env -f docker-compose.yml -f docker-compose.dev.yml down
+        docker build --build-arg envi=dev --tag=vid-grenier-dev .
         docker compose --env-file .env -f docker-compose.yml -f docker-compose.dev.yml up -d
     fi
 }
